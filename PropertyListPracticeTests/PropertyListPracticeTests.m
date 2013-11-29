2e03aab8ae819ebdf57513455e1fe4a327527d08
jamie sa <james.sa.2013@gmail.com>
2013-11-29 16:01:56 +0800

    Step 2

--- PropertyListPracticeTests/PropertyListPracticeTests.m
+++ PropertyListPracticeTests/PropertyListPracticeTests.m
@@ -10,39 +10,93 @@
 #import "Airplane.h"
 
 @interface PropertyListPracticeTests : XCTestCase {
-  Airplane *anAirplane;
+  NSURL *filePath;
 }
+
+@property (strong, nonatomic) Airplane *airplane;
+
+- (void)saveToURL: (NSURL*)url;
+- (void)readFromURL: (NSURL*)url;
 
 @end
 
 @implementation PropertyListPracticeTests
 
+- (void)saveToURL:(NSURL *)url {
+  id plist = [_airplane propertyListRepresentation];
+  NSError *error;
+  NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:plist format:NSPropertyListBinaryFormat_v1_0 options:0 error:&error];
+  if (!plistData) {
+    NSLog(@"Unable to generate plist from Airplane: %@", error);
+  }
+  
+  BOOL success = [plistData writeToURL:url options:NSDataWritingAtomic error:&error];
+  if (!success) {
+    NSLog(@"Unable to write plist data to disk: %@", error);
+  }
+}
+
+- (void)readFromURL:(NSURL *)url {
+  NSError *error;
+  NSData *plistData = [NSData dataWithContentsOfURL:url options:0 error:&error];
+  if (!plistData) {
+    NSLog(@"Unable to read plist data from disk: %@", error);
+    return;
+  }
+  id plist = [NSPropertyListSerialization propertyListWithData:plistData options:0 format:NULL error:&error];
+  if (!plist) {
+    NSLog(@"Unable dto decode plist from data: %@", error);
+    return;
+  }
+  _airplane = [Airplane airplaneWithPropertyListRepresentation:plist];
+}
+
+#pragma marke - UnitTest
+
 - (void)setUp
 {
   [super setUp];
   // Put setup code here. This method is called before the invocation of each test method in the class.
-  anAirplane = [[Airplane alloc]init];
-  anAirplane.model = @"Model default";
-  anAirplane.registrationNumber = @"00000";
-  anAirplane.airframeHours = 0;
+  _airplane = [[Airplane alloc]init];
+  _airplane.model = @"Model default";
+  _airplane.registrationNumber = @"00000";
+  _airplane.airframeHours = 0;
+  filePath = [NSURL fileURLWithPath:@"airport"];
+
 }
 
 - (void)tearDown
 {
-  anAirplane = nil;
+  _airplane = nil;
   [super tearDown];
 }
 
 - (void)testPropertyListRepresentation
 {
-  id shouldBeDict = [anAirplane propertyListRepresentation];
+  id shouldBeDict = [_airplane propertyListRepresentation];
   
   XCTAssertTrue([shouldBeDict isKindOfClass:[NSDictionary class]],@"should be dictionary");
 }
 
 - (void)testGeneratePropertyListRepresentation {
-  Airplane *airplane = [Airplane airplaneWithPropertyListRepresentation:[anAirplane propertyListRepresentation]];
+  Airplane *anAirplane = [Airplane airplaneWithPropertyListRepresentation:[_airplane propertyListRepresentation]];
   
-  XCTAssertEqual(airplane.model, anAirplane.model, @"should be the same");
+  XCTAssertEqual(_airplane.model, anAirplane.model, @"should be the same");
+}
+
+- (void)testSave {
+  [self saveToURL:filePath];
+  
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  
+  XCTAssertTrue([fileManager fileExistsAtPath:[filePath path]], @"file should exist");
+}
+
+- (void)testLoad {
+  _airplane.model = @"Model Saved";
+  [self saveToURL:filePath];
+  _airplane.model = @"Model Default";
+  [self readFromURL:filePath];
+  XCTAssertEqualObjects(_airplane.model, @"Model Saved", @"should be saved");
 }
 @end
